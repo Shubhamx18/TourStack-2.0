992c15c5fac896530e4036734505e7aeac55bc42
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-03-01 19:10:22 +0530

    Create instance.tf

---
+++ terraform/instance.tf
@@ -0,0 +1,55 @@
+resource "aws_security_group" "app_sg" {
+  name = "weather-app-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP"
+  }
+
+  ingress {
+    from_port   = 30007
+    to_port     = 30007
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "NodePort"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_ec2" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install docker -y
+              systemctl start docker
+              systemctl enable docker
+              usermod -aG docker ec2-user
+              EOF
+
+  tags = {
+    Name = "weather-app-ec2"
+  }
+}
