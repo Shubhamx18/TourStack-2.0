fc9813f2465b1a47991d1be57255ef04e130b24c
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-03-01 15:41:55 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,22 +2,10 @@ variable "aws_region" {
   default = "ap-south-1"
 }
 
-variable "ec2_ami" {
-  default = "ami-0f5ee92e2d63afc18"
+variable "cluster_name" {
+  default = "tourstack-eks"
 }
 
-variable "instance_type" {
-  default = "t3.medium"
-}
-
-variable "volume_size" {
-  default = 20
-}
-
-variable "key_name" {
-  default = "terra-key-ec2-latest"
-}
-
-variable "public_key_path" {
-  default = "terra_key_ec2.pub"
+variable "node_instance_type" {
+  default = "c7i-flex.large"
 }
