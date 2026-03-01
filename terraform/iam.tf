db6cc57c8df63d08dd4cf9fc336b1fdbb7bbacb6
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-03-01 16:24:23 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -11,29 +11,8 @@ resource "aws_iam_role" "eks_cluster_role" {
       Action = "sts:AssumeRole"
     }]
   })
-}
 
-resource "aws_iam_role_policy_attachment" "cluster_admin" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-resource "aws_iam_role" "node_group_role" {
-  name = "eks-node-group-role-${var.cluster_name}"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_admin" {
-  role       = aws_iam_role.node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  lifecycle {
+    ignore_changes = all
+  }
 }
