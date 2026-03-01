d4ce83ae5e22df3126112b7759f0efcdae0ef0b3
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-03-01 17:09:33 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,3 +8,27 @@ data "aws_subnets" "default" {
     values = [data.aws_vpc.default.id]
   }
 }
+
+resource "aws_security_group" "eks_nodes_open" {
+  name        = "eks-nodes-open-${var.cluster_name}"
+  description = "Allow all traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-nodes-open-${var.cluster_name}"
+  }
+}
