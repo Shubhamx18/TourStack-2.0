2073431901fa6833a22fc2a6742a3f2bdad176e3
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-03-01 16:41:41 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,6 +1,7 @@
 resource "aws_eks_cluster" "main" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
+  name                          = var.cluster_name
+  role_arn                      = aws_iam_role.eks_cluster_role.arn
+  bootstrap_self_managed_addons = false
 
   vpc_config {
     subnet_ids = data.aws_subnets.default.ids
@@ -8,6 +9,12 @@ resource "aws_eks_cluster" "main" {
 
   lifecycle {
     prevent_destroy = true
-    ignore_changes  = [role_arn]
+    ignore_changes  = [
+      role_arn,
+      bootstrap_self_managed_addons,
+      version,
+      tags,
+      access_config,
+    ]
   }
 }
