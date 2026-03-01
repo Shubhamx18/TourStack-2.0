c6ed07c73a988fd1e70366584dd49f2cfc68eddf
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-03-01 16:27:53 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,4 @@
+# Cluster Role
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role-${var.cluster_name}"
 
@@ -15,4 +16,36 @@ resource "aws_iam_role" "eks_cluster_role" {
   lifecycle {
     ignore_changes = all
   }
+}
+
+# Cluster Admin Access
+resource "aws_iam_role_policy_attachment" "cluster_admin" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# Node Role
+resource "aws_iam_role" "node_group_role" {
+  name = "eks-node-group-role-${var.cluster_name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+# Node Admin Access
+resource "aws_iam_role_policy_attachment" "node_admin" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
