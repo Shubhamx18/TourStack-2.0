da53da47cd86378101c891254f61b71cd700e324
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-03-01 15:44:08 +0530

    Create eks.tf

---
+++ terraform/eks.tf
@@ -0,0 +1,12 @@
+resource "aws_eks_cluster" "main" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.default.ids
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_policy
+  ]
+}
