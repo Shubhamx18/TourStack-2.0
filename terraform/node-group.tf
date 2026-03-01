54a29ce5313f05be8e14a4275d9536be8111ec29
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-03-01 15:44:22 +0530

    Create node-group.tf

---
+++ terraform/node-group.tf
@@ -0,0 +1,20 @@
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "tourstack-nodes"
+  node_role_arn   = aws_iam_role.node_group_role.arn
+  subnet_ids      = data.aws_subnets.default.ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 1
+  }
+
+  instance_types = [var.node_instance_type]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.worker_node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.registry_policy
+  ]
+}
