f08d79bffc8f32ea663dde2bbc35e2fc008fb29c
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-03-01 16:32:00 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,7 +1,33 @@
-data "aws_iam_role" "eks_cluster_role" {
+resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role-${var.cluster_name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "eks.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
-data "aws_iam_role" "node_group_role" {
+resource "aws_iam_role" "node_group_role" {
   name = "eks-node-group-role-${var.cluster_name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "ec2.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
